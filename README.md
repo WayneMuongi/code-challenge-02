889ccc55f43cfc278f0e024432b8168fa1b3adcb
WayneMuongi <waynemuongi77@gmail.com>
2025-06-23 13:54:46 +0300

    challenge two

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# code-challenge-02
+
+# EVENT GUEST LIST MANAGER
+This is simple and interactive web application built using CSS,HTML and Javascript. Users are able to add guest names,remove the guest.The guest limit is 10 people maximum.
+
+
+
+
+
+
+
+## Features
+
+- Limitation of ten guests only.
+- Removal of guests from the list.
+- Addition of a guest to the list.
+
+
+
+## Usage
+Open the index.html and right click to open with live server using your preffered browser e.g google chrome.
+Add a name to the guest list and press add guest.
+## Acknowledgements
+
+This particular project is for educational purposes and the instruction of Moringa School.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
+const button = document.getElementById('btn');
+
+button.addEventListener('click', function(event) {
+        event.preventDefault();
+        const listItem = document.createElement('li');
+        const guestNames = document.getElementById('guestName').value;
+        const button2 = document.createElement('button');
+        button2.textContent = 'Remove';
+        button2.addEventListener('click', function() {
+           listItem.remove();
+    });
+        listItem.textContent = guestNames;
+        listItem.appendChild(button2);
+        document.getElementById('guests').appendChild(listItem);
+        document.getElementById('guestName').value = '';
+        if (document.getElementById('guests').children.length >= 10) {
+            alert('Guest list is full!');
+        }
+
+    const rsvpBtn = document.createElement("button");
+    rsvpBtn.textContent = "Not Attending";
+    rsvpBtn.addEventListener("click", function (){
+      if (rsvpBtn.textContent === "Not Attending") {
+        rsvpBtn.textContent = "Attending";
+
+      } else {
+        rsvpBtn.textContent = "Not Attending";
+
+      }
+   });
+   listItem.appendChild(rsvpBtn);
+});
 
